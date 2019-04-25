051b6cf971977d95079204e0b425e8f02abda70c
Jerome <arvinblureple@163.com>
2019-04-25 21:29:30 +0800

    refactor ver01 StringBuilder和格式化

--- src/main/java/chapter01/ver01/Customer.java
+++ src/main/java/chapter01/ver01/Customer.java
@@ -4,11 +4,16 @@ import java.util.Enumeration;
 import java.util.Vector;
 
 public class Customer {
-    public static final String PRE_AMOUNT_LOG_STR = "Amount owed is ";
-    public static final String PRE_FRE_RENT_POINT_LOG_STR = "You earned ";
-    public static final String POST_AMOUNT_LOG_STR = System.getProperty("line.separator");
-    public static final String POST_FRE_RENT_POINT_LOG_STR = " frequent renter points";
+    private static final String PRE_AMOUNT_LOG_STR = "Amount owed is ";
+
+    private static final String PRE_FRE_RENT_POINT_LOG_STR = "You earned ";
+
+    private static final String POST_AMOUNT_LOG_STR = System.getProperty("line.separator");
+
+    private static final String POST_FRE_RENT_POINT_LOG_STR = " frequent renter points";
+
     private String name; // 姓名
+
     private Vector rentals = new Vector(); // 租借记
 
     public Customer(String name) {
@@ -26,7 +31,8 @@ public class Customer {
     public String statement() {
         double totalAmount = 0; // 总消费金。
         int frequentRenterPoints = 0; // 常客积点
-        String result = "Rental Record for " + getName() + System.getProperty("line.separator");
+        StringBuilder result =
+                new StringBuilder("Rental Record for " + getName() + System.getProperty("line.separator"));
         Enumeration rentals = this.rentals.elements();
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement(); // 取得一笔租借记。
@@ -35,13 +41,13 @@ public class Customer {
             thisAmount = each.getAmount();
             frequentRenterPoints = each.getFrequentRenterPoints(frequentRenterPoints);
             // show figures for this rental（显示此笔租借记录）
-            result += each.printRentalRecord(thisAmount);
+            result.append(each.printRentalRecord(thisAmount));
             totalAmount += thisAmount;
         }
         // add footer lines（结尾打印）
-        result += printLog(totalAmount, PRE_AMOUNT_LOG_STR, POST_AMOUNT_LOG_STR);
-        result += printLog(frequentRenterPoints, PRE_FRE_RENT_POINT_LOG_STR, POST_FRE_RENT_POINT_LOG_STR);
-        return result;
+        result.append(printLog(totalAmount, PRE_AMOUNT_LOG_STR, POST_AMOUNT_LOG_STR))
+                .append(printLog(frequentRenterPoints, PRE_FRE_RENT_POINT_LOG_STR, POST_FRE_RENT_POINT_LOG_STR));
+        return result.toString();
     }
 
     private String printLog(double value, String preStr, String postStr) {
