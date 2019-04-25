2ca45677adc9710c49f1e5b31ab97f51b95007fb
Jerome <arvinblureple@163.com>
2019-04-25 20:30:05 +0800

    refactor ver01

--- src/main/java/chapter01/ver01/Movie.java
+++ src/main/java/chapter01/ver01/Movie.java
@@ -1,27 +1,27 @@
 package chapter01.ver01;
 
 public class Movie {
-	public static final int CHILDRENS = 2;
+	public static final int CHILDREN = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
-	private String _title;
-	private int _priceCode;
+	private String title;
+	private int priceCode;
 
 	public Movie(String title, int priceCode) {
-		_title = title;
-		_priceCode = priceCode;
+		this.title = title;
+		this.priceCode = priceCode;
 	}
 
 	public int getPriceCode() {
-		return _priceCode;
+		return priceCode;
 	}
 
 	public String getTitle() {
-		return _title;
+		return title;
 	}
 
 	public void setPriceCode(int priceCode) {
-		_priceCode = priceCode;
+		this.priceCode = priceCode;
 	}
 
 }
--- src/main/java/chapter01/ver01/Rental.java
+++ src/main/java/chapter01/ver01/Rental.java
@@ -1,19 +1,50 @@
 package chapter01.ver01;
 
 public class Rental {
-	private Movie _movie; // 影片
-	private int _daysRented; // 租期
+	private Movie movie; // 影片
+	private int daysRented; // 租期
 
 	public Rental(Movie movie, int daysRented) {
-		_movie = movie;
-		_daysRented = daysRented;
+		this.movie = movie;
+		this.daysRented = daysRented;
 	}
 
 	public int getDaysRented() {
-		return _daysRented;
+		return daysRented;
 	}
 
 	public Movie getMovie() {
-		return _movie;
+		return movie;
+	}
+
+	public double getAmount() {
+		double thisAmount = 0;
+		switch (getMovie().getPriceCode()) { // 取得影片出租价格
+			case Movie.REGULAR: // 普通片
+				thisAmount += 2;
+				if (getDaysRented() > 2)
+					thisAmount += (getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE: // 新片
+				thisAmount += getDaysRented() * 3;
+				break;
+			case Movie.CHILDREN: // 儿童。
+				thisAmount += 1.5;
+				if (getDaysRented() > 3)
+					thisAmount += (getDaysRented() - 3) * 1.5;
+				break;
+		}
+		return thisAmount;
+	}
+
+	int getFrequentRenterPoints(int frequentRenterPoints) {
+		// add frequent renter points （累计常客积点。
+		frequentRenterPoints++;
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& getDaysRented() > 1) {
+			frequentRenterPoints++;
+		}
+		return frequentRenterPoints;
 	}
 }
